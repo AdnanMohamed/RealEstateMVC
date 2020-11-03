dfa530515a017bd2b9e840c25d32294fa865738c
Adnan Mohamed <amohame6@kent.edu>
2020-11-03 12:25:11 +0300

    pushing files

---
+++ RealEstate/RealEstate/Controllers/CustomersController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RealEstate.Models;
+using RealEstate.Repository;
+
+namespace RealEstate.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private readonly CustomersRepository customersDB;
+
+        public CustomersController()
+        {
+            customersDB = new CustomersRepository();
+        }
+        public string Index()
+        {
+            return "Hello From Customers!";
+        }
+
+        public List<Customer> getAllCustomers()
+        {
+            return customersDB.getCustomers();
+        }
+    }
+}
---
+++ RealEstate/RealEstate/Repository/CustomersRepository.cs
@@ -0,0 +1,34 @@
+﻿using RealEstate.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstate.Repository
+{
+    public class CustomersRepository
+    {
+
+        public CustomersRepository()
+        {
+            Customers = new List<Customer>()
+            {
+                new Customer(){Id = 1, Name= "Adnan Mohamed", Email="adnan@gmail.com"},
+                new Customer(){Id = 2, Name="Ebraheem Nezar", Email="ibra@gmail.com"},
+                new Customer(){Id = 3, Name="Ali Yaseen", Email="ali@gmail.com"},
+                new Customer(){Id = 4, Name="Biliardo Davinchi", Email="bil@gmail.com"}
+            };
+        }
+        public List<Customer> getCustomers()
+        {
+            return Customers;
+        }
+
+        public Customer getCustomer(int id)
+        {
+            return Customers.Where(customer => customer.Id == id).FirstOrDefault();
+        }
+
+        private List<Customer> Customers;
+    }
+}
